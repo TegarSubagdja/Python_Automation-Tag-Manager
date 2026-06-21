e0fbec9bebd9c891b1322a7cd8c1e190f825d376
Tegar Subagdja <kingtegar1510@gmail.com>
2026-06-21 22:01:54 +0700

    all works

--- appOpenChrome.py
+++ appOpenChrome.py
@@ -1,6 +1,7 @@
-import time
 import config
+import time
 import subprocess
+import config
 
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.options import Options
@@ -23,17 +24,16 @@ def connectChrome():
     options.debugger_address = "127.0.0.1:9222"
     return webdriver.Chrome(options=options)
 
+def findTab(target, driver):
+    tabs = driver.window_handles
 
-if __name__ == "__main__":
-    openChrome()
-    time.sleep(5)
-
-    driver = connectChrome()
-    driver.get(
-        "https://tagmanager.google.com/#/view_issue?issueType=7&accountId=6050129313&containerId=89256334"
-    )
-
-    time.sleep(10)
+    for i, tab in enumerate(tabs):
+        driver.switch_to.window(tab)
+        if target in driver.current_url:
+            return tab
+    return None
+        
+def getFirstRow(driver):
 
     rows = driver.find_elements(By.CSS_SELECTOR, "tbody tr")
     print(f"Jumlah row adalah {len(rows)}")
@@ -42,10 +42,9 @@ if __name__ == "__main__":
     for row in rows:
         cols = row.find_elements(By.TAG_NAME, "td")
         if cols:
-            data.append({
-                "url": cols[1].text,
-                "status": cols[2].text
-            })
+            
+            for i, col in enumerate(cols):
+                print(f"Col {i} : {col.text}")
 
             print(f"Click {cols[1].text}")
             ActionChains(driver).move_to_element(row).perform()
@@ -53,7 +52,20 @@ if __name__ == "__main__":
                 lambda r: r.find_element(By.CSS_SELECTOR, "button.icon--button")
             )
             button.click()
-            exit()
+            break
 
-    print(data)
 
+if __name__ == "__main__":
+    # Membuka chrome debugger 
+    # openChrome()
+    # time.sleep(5)
+
+    # Connect ke chrome debugger
+    driver = connectChrome()
+
+    # Membuka halaman
+    # driver.get(
+    #     config.BASE_URL
+    # )
+
+    getFirstRow(driver)
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 ADDRESS_IP = "127.0.0.1:9222"
 
 EXCEL_FILE = "data.xlsx"
+
+BASE_URL = "https://tagmanager.google.com/#/container/accounts/6050129313/containers/89256334/workspaces/13"
