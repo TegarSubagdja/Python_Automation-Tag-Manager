7fb3facf49a329b7c4c7a641222b4581db0f8964
Tegar Subagdja <kingtegar1510@gmail.com>
2026-06-22 13:22:58 +0700

    automated open the url to table

--- appOpenChrome.py
+++ appOpenChrome.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-def openChrome():
+def open_chrome():
     subprocess.Popen([
         r"C:\Program Files\Google\Chrome\Application\chrome.exe",
         "--remote-debugging-port=9222",
@@ -19,12 +19,12 @@ def openChrome():
     ])
 
 
-def connectChrome():
+def connect_chrome():
     options = Options()
     options.debugger_address = "127.0.0.1:9222"
     return webdriver.Chrome(options=options)
 
-def findTab(target, driver):
+def find_tab(target, driver):
     tabs = driver.window_handles
 
     for i, tab in enumerate(tabs):
@@ -33,8 +33,11 @@ def findTab(target, driver):
             return tab
     return None
         
-def getFirstRow(driver):
-    rows = driver.find_elements(By.CSS_SELECTOR, "tbody tr")
+def get_first_row(driver):
+    rows = driver.find_elements(
+        By.CSS_SELECTOR,
+        "tbody tr"
+    )
     print(f"Panjang row adalah {len(rows)}")
     row = rows[3]
     
@@ -43,10 +46,34 @@ def getFirstRow(driver):
         print(f"Col {i} : {col.text}")
     
     try:
-        btn = row.find_element(By.CSS_SELECTOR, "button[title='Open in Tag Assistant']")
+        btn = row.find_element(
+            By.CSS_SELECTOR,
+            "button[title='Open in Tag Assistant']"
+        )
         ActionChains(driver, 1000).move_to_element(btn).click(btn).perform()
     except Exception as e:
         print(f"Button tidak ditemukan di row ini : {e}")
+
+def get_view_issue(driver):
+    btn = driver.find_element(
+        By.CSS_SELECTOR,
+        "button.wd-issues-button"
+    )
+    ActionChains(driver, 1000).move_to_element(btn).click(btn).perform()
+
+def see_untaged_pages(driver):
+    btn = driver.find_element(
+        By.CSS_SELECTOR,
+        "a.wd-actionItem__action"
+        )
+    ActionChains(driver, 1000).move_to_element(btn).click(btn).perform()
+
+def get_tagged_pages(driver):
+    btn = driver.find_element(
+        By.CSS_SELECTOR,
+        "gtm-popover[aria-label='More information about included pages']"
+    )
+    ActionChains(driver, 1000).move_to_element(btn).click(btn).perform()
 
 if __name__ == "__main__":
     # Membuka chrome debugger 
@@ -54,11 +81,17 @@ if __name__ == "__main__":
     # time.sleep(5)
 
     # Connect ke chrome debugger
-    driver = connectChrome()
+    driver = connect_chrome()
 
     # Membuka halaman
     driver.get(
         config.BASE_URL
     )
 
-    getFirstRow(driver)
+    get_view_issue(driver)
+    time.sleep(3)
+
+    see_untaged_pages(driver)
+    time.sleep(3)
+
+    # getFirstRow(driver)
